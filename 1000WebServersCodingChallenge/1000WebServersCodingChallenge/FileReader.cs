cda9bc718359a3e1c9e3256fa520f2a8e8fdca07
baharmohammadi68 <bahar.mohammadi68@gmail.com>
2020-12-28 23:40:16 -0800

    initial commit

---
+++ 1000WebServersCodingChallenge/1000WebServersCodingChallenge/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1000WebServersCodingChallenge
+{
+    public class FileReader
+    {
+        #region Methods
+        public static List<string> ReadFile(string filePath)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                lines = System.IO.File.ReadAllLines(filePath)?.ToList();
+            }
+            catch(Exception ex)
+            {
+                //todo:add logging message
+                Console.WriteLine($"Failed to read the list of servers. {ex.Message}");
+            }
+            return lines;
+        }
+        #endregion
+    }
+}
---
+++ 1000WebServersCodingChallenge/1000WebServersCodingChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace _1000WebServersCodingChallenge
+{
+    class Program
+    {
+        #region
+        public const string filePath = ""; //todo: make this constant in app setting and read from there
+        //todo: make a const file for success/error/info message
+        #endregion
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Reading the list of 1000 servers!");
+            List<string> servers = FileReader.ReadFile(filePath);
+            await CreateHTTPRequests(servers);
+        }
+
+        private static async Task CreateHTTPRequests(List<string> urls)
+        {
+            var client = new HttpClient();
+
+            //Start requests for all of them
+            var requests = urls.Select
+                (
+                    url => client.GetAsync(url)
+                ).ToList();
+
+            //Wait for all the requests to finish
+            await Task.WhenAll(requests);
+
+            //Get the responses
+            var responses = requests.Select
+                (
+                    task => task.Result
+                );
+
+            foreach (var r in responses)
+            {
+                // Extract the message body
+                var s = await r.Content.ReadAsStringAsync();
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+
